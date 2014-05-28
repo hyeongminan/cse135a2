2b6074cea1e08b70e32fc30e3c0cb2fd0c2765e8
hyeongminan <hyeongminan@gmail.com>
2014-05-27 17:24:04 -0700

    Changed util databse info to local info

--- src/cse135/Util.java
+++ src/cse135/Util.java
@@ -5,11 +5,17 @@ import javax.servlet.http.*;
 
 public class Util {
 
-	
+	/*
 	public static final String SERVERNAME = "ec2-54-187-115-171.us-west-2.compute.amazonaws.com";
 	public static final String USERNAME = "ubuntu";
 	public static final String PASSWORD = "ubuntu";
 	public static final String DATABASE = "cse135";
+	public static final String PORTNUMBER = "5432";
+	*/
+	public static final String SERVERNAME = "localhost";
+	public static final String USERNAME = "postgres";
+	public static final String PASSWORD = "postgres";
+	public static final String DATABASE = "P1";
 	public static final String PORTNUMBER = "5432";
 	
 	
